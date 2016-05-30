6acd18acdd448aed3ccd867e00b7dbda04ab3e20
Nikita Ignatov <hi@nikitaignatov.com>
2016-05-30 09:56:06 +0200

    repo changes: added register, connection failure notifications

--- api/repo.js
+++ api/repo.js
@@ -1,17 +1,34 @@
 
 var connection = $.hubConnection('http://localhost:9000');
 var proxy = connection.createHubProxy('nfcHub');
+import {changeSettingsComplete,recieveUpdate,logFromServer,connected,connectionFailed} from '../actions'
+import {toastr} from 'react-redux-toastr'
+
 
 proxy.on('update', function(message) {
     console.log(message);
 });
+proxy.on('log', function(message) {
+    logFromServer(message);
+});
+proxy.on('changeSettingsComplete', function(data) {
+  changeSettingsComplete(data)
+});
+
+proxy.on('register', function(data) {
+  console.log('who is:', data)
+  proxy.invoke('register', data, prompt('Who is this?', '')).done((e)=> console.log('registered',e));
+});
 
 connection.start({ jsonp: false })
-  .done(function(){ console.log('Now connected, connection ID=' + connection.id); })
-  .fail(function(e){ console.log('Could not connect',e); });
+  .done(function(){ console.log('Now connected, connection ID=' + connection.id); connected()  })
+  .fail(function(e){
+    console.log('Could not connect',e);
+    connectionFailed('Could not connect to server.')
+  });
 
 export default {
-  getSessions(call) {
+  recieveUpdate(call) {
     proxy.on('update', function(data) {
       console.log('update',data)
       call(data)
@@ -20,11 +37,14 @@ export default {
   update(data,call) {
     proxy.invoke('commentOn', data.id, data.message, data.ticket, data.type).done((e)=> call(e));
   },
-  submitTime(id,call) {
-    console.log('submit',id)
-    proxy.invoke('submitTimeEntry', id).done((e)=> call(e));
+  submitTime(id,who,call) {
+    console.log('submit',id,who)
+    proxy.invoke('submitTimeEntry', id,who).done((e)=> call(e));
   },
   remove(id,call) {
     proxy.invoke('remove', id).done((e)=> call(e));
+  },
+  changeSettings(settings,call) {
+    proxy.invoke('changeSettings', settings).done((e)=> console.log('changeSettings done',e));
   }
 }
