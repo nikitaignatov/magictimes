b7e1be12a6701217157827683205e6ee852c0a19
Nikita Ignatov <hi@nikitaignatov.com>
2016-06-30 08:07:27 +0200

    changed the port and added the event handler

--- api/repo.js
+++ api/repo.js
@@ -5,8 +5,8 @@ import {registerUser} from '../actions/users'
 import swal from 'sweetalert2'
 
 export function startSignlar(store) {
-  var connection = $.hubConnection('http://localhost:9000');
-  var proxy = connection.createHubProxy('nfcHub');
+  var connection = $.hubConnection('http://localhost:8085');
+  var proxy = connection.createHubProxy('commandHub');
 
   proxy.on('dummy', message => store.dispatch({type:'DUMMY',message}));
 
@@ -15,6 +15,8 @@ export function startSignlar(store) {
     .done(() => store.dispatch(serverConnected(proxy, connection.id)))
     .fail((e)=> store.dispatch(serverConnectionFailed(e)));
 
+  var normalize = (event) => (event && event.data && event.data.length == 1 ? {type:event.type, data:event.data[0]}:{type:'EMPTY_EVENT'})
+  proxy.on('event', (data)=> store.dispatch(normalize(data)));
   proxy.on('update', (data)=> store.dispatch({type:'RECIEVE_UPDATE',data}));
   proxy.on('log', (message) => store.dispatch(serverErrorLog(message)));
   proxy.on('changeSettingsComplete', (data) => store.dispatch(changeSettingsComplete(data)));
